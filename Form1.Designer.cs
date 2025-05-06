0a3b56f5014c195e1977b897b0c8cb9233bb43ed
Sevban Bayır <sevbanbuyer@gmail.com>
2025-05-06 20:17:49 +0300

    create list search & replace app

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.replaceTextBox = new System.Windows.Forms.TextBox();
+            this.replaceButton = new System.Windows.Forms.Button();
+            this.listLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 30);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.PlaceholderText = "Enter number to search";
+            this.searchTextBox.Size = new System.Drawing.Size(150, 23);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(168, 29);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 65);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(39, 15);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "Status";
+            // 
+            // replaceTextBox
+            // 
+            this.replaceTextBox.Location = new System.Drawing.Point(12, 100);
+            this.replaceTextBox.Name = "replaceTextBox";
+            this.replaceTextBox.PlaceholderText = "Enter new number";
+            this.replaceTextBox.Size = new System.Drawing.Size(150, 23);
+            this.replaceTextBox.TabIndex = 3;
+            // 
+            // replaceButton
+            // 
+            this.replaceButton.Location = new System.Drawing.Point(168, 99);
+            this.replaceButton.Name = "replaceButton";
+            this.replaceButton.Size = new System.Drawing.Size(75, 23);
+            this.replaceButton.TabIndex = 4;
+            this.replaceButton.Text = "Replace";
+            this.replaceButton.UseVisualStyleBackColor = true;
+            this.replaceButton.Click += new System.EventHandler(this.ReplaceButton_Click);
+            // 
+            // listLabel
+            // 
+            this.listLabel.AutoSize = true;
+            this.listLabel.Location = new System.Drawing.Point(12, 9);
+            this.listLabel.Name = "listLabel";
+            this.listLabel.Size = new System.Drawing.Size(70, 15);
+            this.listLabel.TabIndex = 5;
+            this.listLabel.Text = "Current List:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(260, 150);
+            this.Controls.Add(this.listLabel);
+            this.Controls.Add(this.replaceButton);
+            this.Controls.Add(this.replaceTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "Form1";
+            this.Text = "List Search & Replace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox replaceTextBox;
+        private System.Windows.Forms.Button replaceButton;
+        private System.Windows.Forms.Label listLabel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,76 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace ListSearchAndReplaceApp
 {
     public partial class Form1 : Form
     {
+        private List<int> numbers = new List<int> { 10, 20, 30, 40, 50, 60, 70 };
+        private int? lastSearchedValue = null;
+        private int lastFoundIndex = -1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateListLabel();
+            resultLabel.Text = "App loaded.";
+        }
+
+        private void UpdateListLabel()
+        {
+            listLabel.Text = "Current List: " + string.Join(", ", numbers);
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            lastSearchedValue = null;
+            lastFoundIndex = -1;
+            if (int.TryParse(searchTextBox.Text, out int searchValue))
+            {
+                lastSearchedValue = searchValue;
+                lastFoundIndex = numbers.IndexOf(searchValue);
+                if (lastFoundIndex != -1)
+                {
+                    resultLabel.Text = $"Found {searchValue} at index {lastFoundIndex}.";
+                }
+                else
+                {
+                    resultLabel.Text = $"{searchValue} not found.";
+                }
+            }
+            else
+            {
+                resultLabel.Text = "Invalid search input. Enter a number.";
+            }
+        }
+
+        private void ReplaceButton_Click(object sender, EventArgs e)
+        {
+            if (lastSearchedValue == null || lastFoundIndex == -1)
+            {
+                resultLabel.Text = "Please search for a value first.";
+                return;
+            }
+
+            if (int.TryParse(replaceTextBox.Text, out int replaceValue))
+            {
+                numbers[lastFoundIndex] = replaceValue;
+                resultLabel.Text = $"Replaced {lastSearchedValue} with {replaceValue}.";
+                UpdateListLabel();
+                searchTextBox.Text = string.Empty;
+                replaceTextBox.Text = string.Empty;
+                lastSearchedValue = null;
+                lastFoundIndex = -1;
+            }
+            else
+            {
+                resultLabel.Text = "Invalid replace input. Enter a number.";
+            }
         }
     }
 }
